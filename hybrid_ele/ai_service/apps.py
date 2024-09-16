1223429654ef3fc88dad5a326d3942a5c03aa259
Aditya Koul <addy.adi@hotmail.com>
2024-09-16 17:12:36 +0530

    removiing django related stuff

--- hybrid_ele/ai_service/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AiServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ai_service'
--- hybrid_ele/learning_path_service/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LearningPathServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'learning_path_service'
