6530e63abcd912ed39a91b7da16262e91e7fd320
sushen <sushenydv@gmail.com>
2024-09-17 17:39:02 +0530

    Fixed naming error

--- v1/backend/core/pdf_pipeline/chunking.py
+++ v1/backend/core/pdf_pipeline/chunking.py
@@ -8,11 +8,11 @@ def chunk_pdf(file_path, out_path):
     """
     Chunks a PDF file into smaller segments
     """
-    md_text = pymupdf4llm.to_markdown("filename")
+    md_text = pymupdf4llm.to_markdown(file_path)
     pathlib.Path(f"{out_path}/output.md").write_bytes(md_text.encode())
     splitter = MarkdownTextSplitter(chunk_size=40, chunk_overlap=0)
     splitter.create_documents([md_text])
-    pass
     
+    return 0
 
-
+chunk_pdf(file_path="/home/tso/hybrid_ai_personalised_learning/v1/backend/core/pdf_pipeline/input.pdf",out_path="v1/backend/core/pdf_pipeline")
